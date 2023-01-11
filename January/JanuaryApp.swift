d9d564c0fe8c2d51c37ddf2563bcd00ca976f7a4
Evan Metrock <evanmetrock@Evans-iMac.local>
2023-01-10 21:58:27 -0600

    update

---
+++ January/JanuaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  JanuaryApp.swift
+//  January
+//
+//  Created by Evan Metrock on 1/10/23.
+//
+
+import SwiftUI
+
+@main
+struct JanuaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
